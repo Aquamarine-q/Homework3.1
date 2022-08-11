13509da473ca9ede1a49a1e37ec26cbf751a7980
Darya-aq <momo.darya@mail.ru>
2022-08-12 02:11:42 +0600

    create project

--- app/src/main/java/com/example/flags/MainActivity.kt
+++ app/src/main/java/com/example/flags/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.flags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
     }
 }
